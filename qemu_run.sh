547c53b5d89fc7e99cc4907501e22df0d986b884
Daan de Graaf <daan.de.graaf@prodrive-technologies.com>
2019-03-09 14:54:56 +0100

    Run a web server with cached dependencies.

--- qemu_run.sh
+++ qemu_run.sh
@@ -4,5 +4,5 @@ qemu-system-x86_64 \
     -cdrom bazel-genfiles/dist.iso \
     -boot d \
     -nographic \
-    -netdev user,id=n1 \
+    -netdev user,id=n1,hostfwd=tcp::3000-:3000 \
     -device e1000,netdev=n1
--- rust_hello/src/main.rs
+++ rust_hello/src/main.rs
@@ -1,3 +1,23 @@
+extern crate hyper;
+
+use hyper::rt::{self, Future};
+use hyper::service::service_fn_ok;
+use hyper::{Body, Request, Response, Server};
+
 fn main() {
-    println!("Good morning, Vietnam!");
+    println!("Hello, world!");
+    let addr = ([0, 0, 0, 0], 3000).into();
+
+    let server = Server::bind(&addr)
+        .serve(|| {
+            // This is the `Service` that will handle the connection.
+            // `service_fn_ok` is a helper to convert a function that
+            // returns a Response into a `Service`.
+            service_fn_ok(move |_: Request<Body>| Response::new(Body::from("Hello World!\n")))
+        })
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    println!("Listening on http://{}", addr);
+
+    rt::run(server);
 }

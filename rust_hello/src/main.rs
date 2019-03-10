f38697d6a9d7152eb624e4c732172207e92adf72
Daan de Graaf <daan.de.graaf@prodrive-technologies.com>
2019-03-10 13:49:23 +0100

    Setup ip address from rust.

--- rust_hello/src/main.rs
+++ rust_hello/src/main.rs
@@ -1,5 +1,3 @@
-extern crate hyper;
-extern crate libc;
 #[macro_use]
 extern crate nix;
 
@@ -30,7 +28,9 @@ fn main() {
     rt::run(server);
 }
 
+// Set interface flags
 use libc::{c_char, c_short, IFF_UP, IFNAMSIZ, SIOCSIFFLAGS};
+use std::ptr;
 
 #[repr(C)]
 pub struct FlagsRequest {
@@ -38,7 +38,62 @@ pub struct FlagsRequest {
     flags: c_short,
 }
 
+fn interface_name(interface: &str) -> [c_char; IFNAMSIZ] {
+    let mut interface_name = [0; IFNAMSIZ];
+    if interface.len() > IFNAMSIZ - 1 {
+        // TODO should we really panic here, or just return an error?
+        panic!(
+            "Interface name `{}` is too long, maximum length is {} characters",
+            interface,
+            IFNAMSIZ - 1
+        );
+    }
+    // Copy the interface name into the designated array.
+    // This copy is safe, as we have checked that interface.len() is no greater than
+    // IFNAMSIZ - 1.
+    unsafe {
+        ptr::copy(
+            interface.as_ptr() as *const c_char,
+            interface_name.as_mut_ptr() as *mut c_char,
+            interface.len(),
+        )
+    };
+    interface_name
+}
+
+impl FlagsRequest {
+    pub fn new(interface: &str, flags: c_short) -> FlagsRequest {
+        FlagsRequest {
+            interface_name: interface_name(interface),
+            flags,
+        }
+    }
+}
+
 ioctl_write_ptr_bad!(set_interface_flags, SIOCSIFFLAGS, FlagsRequest);
+
+// Set interface address
+ioctl_write_ptr_bad!(set_interface_address, SIOCSIFADDR, AddrRequest);
+
+use libc::{sockaddr, SIOCSIFADDR};
+use socket2::SockAddr;
+use std::net::{IpAddr, SocketAddr};
+
+#[repr(C)]
+pub struct AddrRequest {
+    interface_name: [c_char; IFNAMSIZ],
+    address: sockaddr,
+}
+
+impl AddrRequest {
+    pub fn new(interface: &str, address: IpAddr) -> AddrRequest {
+        let addr = SockAddr::from(SocketAddr::new(address, 0));
+        AddrRequest {
+            interface_name: interface_name(interface),
+            address: unsafe { *addr.as_ptr() },
+        }
+    }
+}
 
 // TODO use `ioctl`s for this
 fn setup_network() {
@@ -50,27 +105,15 @@ fn setup_network() {
     )
     .unwrap();
 
-    let mut interface_name = [0; IFNAMSIZ];
-    interface_name[0] = 'e' as c_char;
-    interface_name[1] = 't' as c_char;
-    interface_name[2] = 'h' as c_char;
-    interface_name[3] = '0' as c_char;
-
-    let req = FlagsRequest {
-        interface_name,
-        flags: IFF_UP as i16,
-    };
+    let req = FlagsRequest::new("eth0", IFF_UP as c_short);
     unsafe { set_interface_flags(sock, &req as *const FlagsRequest).expect("Failed to set flags") };
-    unsafe { libc::close(sock) };
 
-    Command::new("/sbin/ip")
-        .arg("addr")
-        .arg("add")
-        .arg("10.0.2.15")
-        .arg("dev")
-        .arg("eth0")
-        .status()
-        .expect("Add interface address");
+    let req = AddrRequest::new("eth0", "10.0.2.15".parse().unwrap());
+    unsafe {
+        set_interface_address(sock, &req as *const AddrRequest)
+            .expect("Failed to set interface address")
+    };
+    unsafe { libc::close(sock) };
 
     Command::new("/sbin/ip")
         .arg("route")
